80f9f471049b912efab2c8a3d4e32daa94a7806b
David Clark <david.dave.clark@gmail.com>
2016-01-28 07:00:33 -0700

    Initial arrowing idea

---
+++ index.js
@@ -0,0 +1,98 @@
+var activeGroup;
+
+function FocusGroup (options) {
+  this._settings = {
+    forwardArrows: options.forward || ['down'],
+    backArrows: options.prev || ['up'],
+    cycle: options.cycle,
+  }
+  this._nodes = options.initialNodes || [];
+  this._handleKeyDown = this.handleKeyDown.bind(this);
+};
+
+FocusGropup.prototype.activate = function() {
+  if (activeGroup) activeGroup.deactivate();
+  activeGroup = this;
+  document.documentElement.addEventListener('keydown', this._handleKeyDown, true);
+  return this;
+};
+
+FocusGropup.prototype.deactivate = function() {
+  activeGroup = null;
+  document.documentElement.removeEventListener('keydown', this._handleKeyDown, true);
+  return this;
+};
+
+FocusGropup.prototype.handleKeyDown = function(event) {
+  var arrow = getEventArrowKey(event);
+  if (!arrow) return;
+
+  var activeNodeIndex = this._nodes.indexOf(document.activeElement);
+  if (activeNodeIndex === -1) return;
+
+  if (this._settings.forwardArrows.indexOf(arrow) !== -1) {
+    event.preventDefault();
+    return this.moveFocusForward(activeNodeIndex);
+  }
+  if (this._settings.backArrows.indexOf(arrow) !== -1) {
+    event.preventDefault();
+    return this.moveFocusBack(activeNodeIndex);
+  }
+};
+
+FocusGropup.prototype.moveFocusForward = function(activeNodeIndex) {
+  var targetNodeIndex;
+  if (activeNodeIndex < this._nodes.length - 1) {
+    targetNodeIndex = activeNodeIndex + 1;
+  } else if (this._settings.cycle) {
+    targetNodeIndex = 0;
+  } else {
+    targetNodeIndex = activeNodeIndex;
+  }
+  this.focusNodeAtIndex(targetNodeIndex);
+  return targetNodeIndex;
+};
+
+FocusGropup.prototype.moveFocusBack = function(activeNodeIndex) {
+  var targetNodeIndex;
+  if (activeNodeIndex > 0) {
+    targetNodeIndex = activeNodeIndex - 1;
+  } else if (this._settings.cycle) {
+    targetNodeIndex = this._nodes.length - 1;
+  } else {
+    targetNodeIndex = activeNodeIndex;
+  }
+  this.focusNodeAtIndex(targetNodeIndex);
+  return targetNodeIndex;
+};
+
+FocusGropup.prototype.focusNodeAtIndex = function(targetNodeIndex) {
+  var targetNode = this._nodes[targetNodeIndex];
+  if (targetNode && targetNode.focus) targetNode.focus();
+};
+
+FocusGropup.prototype.addNode = function(node) {
+  this._nodes.push(node);
+};
+
+FocusGropup.prototype.removeNode = function(node) {
+  var nodeIndex = this._nodes.indexOf(node);
+  if (nodeIndex === -1) return;
+  this._nodes.splice(nodeIndex, 1);
+};
+
+protoOrder.clearNodes = function() {
+  this._nodes = [];
+};
+
+module.exports = function createArrowOrder(options) {
+  return new FocusGroup(options);
+};
+
+function getEventArrowKey(event) {
+  if (event.key === 'ArrowUp' || event.keyCode === 38) return 'up';
+  if (event.key === 'ArrowDown' || event.keyCode === 40) return 'down';
+  if (event.key === 'ArrowLeft' || event.keyCode === 37) return 'left';
+  if (event.key === 'ArrowRight' || event.keyCode === 39) return 'right';
+  return null;
+}
